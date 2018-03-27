aa47829f7427b490ae254f326bf53d90dea4b41f
山木祥平 <yamaki817122@gmail.com>
2018-03-27 23:30:36 +0900

    add layout and filter

--- myapp.rb
+++ myapp.rb
@@ -1,13 +1,53 @@
 require 'sinatra'
 require "sinatra/reloader"
 
+# ===========================
+# filter
+# ===========================
+
+# bofore filter
+before do
+	@note = "Hi!!"
+	@author = "SyoheiYamaki"
+end
+# after filter
+after do
+	puts response.status
+end
+
+# ===========================
+# helper
+# ===========================
+
+helpers do
+
+	def bar(name)
+		"#{name}_bar"
+	end
+
+end
+
+# ===========================
+# routing
+# ===========================
+
 get '/' do
-	'Hello! World!!'
+	@title = "main"
+	@content = "main content"
+	erb :index
+end
+
+get "/about" do
+	@title = "about"
+	@content = "about content"
+	@email = "syohei@gmail.com"
+	erb :about
 end
 
 get "/hello/:name" do
 	"Hello!! #{params["name"]}"
 end
+
 
 get "/good/:name" do |name|
 	"are you #{name}??"
